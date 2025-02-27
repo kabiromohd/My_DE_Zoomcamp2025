fbeaba3d5a54c632b2709e9592ed529429cb925f
Kabir Mohammed <kabirolawalemohammed@gmail.com>
2025-02-27 22:03:23 +0000

    update

--- Analytics_Engineering_module4/models/staging/stg_staging__fhv_tripdata.sql
+++ Analytics_Engineering_module4/models/staging/stg_staging__fhv_tripdata.sql
@@ -26,4 +26,13 @@ select
 
     SR_Flag
 
-    from tripdata
+from tripdata
+where rn = 1
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=false) %}
+
+  --limit 100
+
+{% endif %}
